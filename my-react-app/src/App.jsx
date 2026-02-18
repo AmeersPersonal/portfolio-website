f371db63e26d0af4e129f2c04e7307d3f4f1a154
Ameer <71401181+AmeersPersonal@users.noreply.github.com>
2026-02-17 21:36:38 -0500

    getting rid of 404 errors

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,9 +1,8 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 import NavBar from './components/navBar'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 
 
 import "./pages/home.jsx"
